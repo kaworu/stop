2266d185e05cdf5c5e6c4bc14d82df97bd3b0f07
Alexandre "kAworu" Perrin <kaworu(a)kaworu,ch>
2009-07-11 21:33:00 +0200

    reverted BatteryMeter as close as original as possible

--- BatteryMeter.c
+++ BatteryMeter.c
@@ -29,6 +29,89 @@ int BatteryMeter_attributes[] = {
    BATTERY
 };
 
+static unsigned long int parseUevent(FILE * file, char *key) {
+   char line[100];
+   unsigned long int dValue = 0;
+
+   while (fgets(line, sizeof line, file)) {
+      if (strncmp(line, key, strlen(key)) == 0) {
+         char *value;
+         value = strtok(line, "=");
+         value = strtok(NULL, "=");
+         dValue = atoi(value);
+         break;
+      }
+   }
+   return dValue;
+}
+
+static unsigned long int parseBatInfo(const char *fileName, const unsigned short int lineNum, const unsigned short int wordNum) {
+   const DIR *batteryDir;
+   const struct dirent *dirEntries;
+
+   const char batteryPath[] = PROCDIR "/acpi/battery/";
+   batteryDir = opendir(batteryPath);
+
+   if (batteryDir == NULL) {
+      return 0;
+   }
+
+   char *entryName;
+   typedef struct listLbl {
+      char *content;
+      struct listLbl *next;
+   } list;
+
+   list *myList = NULL;
+   list *newEntry;
+
+   /*
+      Some of this is based off of code found in kismet (they claim it came from gkrellm).
+      Written for multi battery use...
+    */
+   for (dirEntries = readdir((DIR *) batteryDir); dirEntries; dirEntries = readdir((DIR *) batteryDir)) {
+      entryName = (char *) dirEntries->d_name;
+
+      if (strncmp(entryName, "BAT", 3))
+         continue;
+
+      newEntry = calloc(1, sizeof(list));
+      newEntry->next = myList;
+      newEntry->content = entryName;
+      myList = newEntry;
+   }
+
+   unsigned long int total = 0;
+   for (newEntry = myList; newEntry; newEntry = newEntry->next) {
+      const char infoPath[30];
+      const FILE *file;
+      char line[50];
+
+      snprintf((char *) infoPath, sizeof infoPath, "%s%s/%s", batteryPath, newEntry->content, fileName);
+
+      if ((file = fopen(infoPath, "r")) == NULL) {
+         return 0;
+      }
+
+      for (unsigned short int i = 0; i < lineNum; i++) {
+         fgets(line, sizeof line, (FILE *) file);
+      }
+
+      fclose((FILE *) file);
+
+      const char *foundNumTmp = String_getToken(line, wordNum);
+      const unsigned long int foundNum = atoi(foundNumTmp);
+      free((char *) foundNumTmp);
+
+      total += foundNum;
+   }
+
+   free(myList);
+   free(newEntry);
+   closedir((DIR *) batteryDir);
+   return total;
+}
+
 static ACPresence chkIsOnline(void) {
     if (Sysctl.getInt("hw.acpi.acline"))
         return (AC_PRESENT);
@@ -36,15 +119,19 @@ static ACPresence chkIsOnline(void) {
         return (AC_ABSENT);
 }
 
-static double getBatData(void) {
+/* still named getProcBatData event if it doesn't use Proc, just to ease merge. */
+static double getProcBatData(void) {
    return ((double)Sysctl.getInt("hw.acpi.battery.life"));
 }
 
 static void BatteryMeter_setValues(Meter * this, char *buffer, int len) {
-   double percent = getBatData();
+   double percent = getProcBatData();
    if (percent == 0) {
+      //percent = getSysBatData();
+      if (percent == 0) {
          snprintf(buffer, len, "n/a");
          return;
+      }
    }
 
    this->values[0] = percent;
